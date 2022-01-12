f036a104aa3b34c75bcfb24457dc8234c4b49d63
Matt Willy <matt.willy@gmail.com>
2022-01-12 10:34:05 -0700

    add tests and fix an error with tags

--- lib/plate_slate/menu/menu.ex
+++ lib/plate_slate/menu/menu.ex
@@ -129,7 +129,7 @@ defmodule PlateSlate.Menu do
     args
     |> Enum.reduce(Item, fn
       {:order, order}, query ->
-        from q in query, order_by: {^order, :name}
+        query |> order_by({^order, :name})
 
       {:filter, filter}, query ->
         query |> filter_with(filter)
@@ -138,30 +138,26 @@ defmodule PlateSlate.Menu do
   end
 
   defp filter_with(query, filter) do
-    Enum.reduce(
-      filter,
-      query,
-      fn
-        {:name, name}, query ->
-          from q in query, where: ilike(q.name, ^"%#{name}%")
+    Enum.reduce(filter, query, fn
+      {:name, name}, query ->
+        from q in query, where: ilike(q.name, ^"%#{name}%")
 
-        {:priced_above, price}, query ->
-          from q in query, where: q.price >= ^price
+      {:priced_above, price}, query ->
+        from q in query, where: q.price >= ^price
 
-        {:priced_below, price}, query ->
-          from q in query, where: q.price <= ^price
+      {:priced_below, price}, query ->
+        from q in query, where: q.price <= ^price
 
-        {:category, category_name}, query ->
-          from q in query,
-            join: c in assoc(q, :category),
-            where: ilike(c.name, ^"%#{category_name}%")
+      {:category, category_name}, query ->
+        from q in query,
+          join: c in assoc(q, :category),
+          where: ilike(c.name, ^"%#{category_name}%")
 
-        {:tage, tag_name}, query ->
-          from q in query,
-            join: t in assoc(q, :tags),
-            where: ilike(t.name, ^"%#{tag_name}%")
-      end
-    )
+      {:tag, tag_name}, query ->
+        from q in query,
+          join: t in assoc(q, :tags),
+          where: ilike(t.name, ^"%#{tag_name}%")
+    end)
   end
 
   @doc """
